e08990bd2d7c4309237f19a0e2efc625e04bbd21
Chia1104 <yuyuchia7423@gmail.com>
2024-01-31 15:01:13 +0800

    db seed

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -18,14 +18,32 @@ if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 import { faker } from "@faker-js/faker";
 
 async function main() {
-  void (await prisma.post.create({
-    data: {
-      slug: faker.lorem.slug(),
-      title: faker.lorem.sentence(),
-      excerpt: faker.lorem.paragraph(),
-      content: faker.lorem.paragraphs(),
-      published: true,
-    },
+  void (await prisma.$transaction(async (tx) => {
+    const cert = await tx.cert.create({
+      data: {
+        image:
+          "https://storage.googleapis.com/echox-business-backend-luxgen-beta/collection/f216cb2c-0a3a-4c06-95ec-b450d9c4c5e4/24/echox-nft.jpg",
+        name: faker.lorem.words(5),
+        tokenId: faker.lorem.words(5),
+        nftUrl:
+          "https://mumbai.polygonscan.com/nft/0xbdcc98de88438b2297365ff45fed85b318bba25e/24",
+        openseaUrl:
+          "https://testnets.opensea.io/assets/matic/0xbdcc98de88438b2297365ff45fed85b318bba25e/24",
+        deactivatedAt: new Date(),
+        isSvcroUnderSyncing: false,
+        lastRecordedAt: new Date(),
+      },
+    });
+
+    return tx.vehicle.create({
+      data: {
+        certId: cert.id,
+        regoNo: faker.lorem.words(5),
+        modelCode: faker.lorem.words(5),
+        extColour: faker.lorem.words(5),
+        intColour: faker.lorem.words(5),
+      },
+    });
   }));
 }
 void main().then(async () => {
